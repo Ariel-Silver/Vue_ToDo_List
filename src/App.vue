372a426a3469a88df6a8afb7912f90e66af0134d
Ariel <Ariel.abarca7@gmail.com>
2021-10-03 18:24:13 -0300

    ultimo commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app" class="text-center">
+    <h1 class="py-3">Crear una nueva tarea</h1>
+    <label class="pr-4">Tarea</label>
+    <input class="me-4 ms-4" type="text" v-model= "Deberes" placeholder="Ingrese nueva tarea"/>
+    <button class="btn btn-dark btn-lg" @click="Agregar">Agregar</button>
+    <ul>
+      <p>To Do:</p>
+      <li v-for="(Tarea, $index) in Tareas" :key="$index">
+        {{Tarea}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: () => ({
+    Deberes: "",
+    Tareas: ["Limpiar arenero de la gata", "botar la basura"]
+  }),
+  methods:{
+    Agregar: function(){
+      this.Tareas.push(this.Deberes)
+      this.Deberes = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+body,html{
+  width: 100%;
+  height: 100%;
+}
+div{
+  background-color: cadetblue;
+  margin: auto;
+  margin-top: 50px;
+  width: 800px;
+  height: 800px;
+  border: solid 3px black;
+}
+
+p {
+  margin-top: 20px;
+  margin-right: 20px;
+  font-size: large;
+  text-decoration: underline;
+}
+
+button {
+  margin-top: 30px;
+  width: 150px;
+  height: 45px;
+  padding-top: 1px;
+} 
+
+input {
+  width: 500px;
+  border: solid 1px black;
+}
+label {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-weight: 600;
+  font-size: 30px;
+}
+
+ul {
+  font-weight:bold;
+}
+
+li {
+  margin-right: 20px;
+}
+</style>
